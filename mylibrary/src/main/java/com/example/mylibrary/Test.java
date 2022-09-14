d4ae274294c51486bd7b7a8b720238fc8b74ba96
dilipgupta123-123 <GDIlip10#$%>
2022-09-14 19:42:42 +0530

    fuuy

---
+++ mylibrary/src/main/java/com/example/mylibrary/Test.java
@@ -0,0 +1,18 @@
+package com.example.mylibrary;
+
+import android.os.Bundle;
+
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+
+public class Test implements IALocationListener {
+    @Override
+    public void onLocationChanged(IALocation iaLocation) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+}
