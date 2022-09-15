744bde1a41eafa0ae7d50e858faa1ef87f7ddb41
dilipgupta123-123 <GDIlip10#$%>
2022-09-15 16:53:39 +0530

    fuuy

--- mylibrary/src/main/java/com/example/mylibrary/Test.java
+++ mylibrary/src/main/java/com/example/mylibrary/Test.java
@@ -1,9 +1,4 @@
 package com.example.mylibrary;
 
-import android.os.Bundle;
-
-import com.indooratlas.android.sdk.IALocation;
-import com.indooratlas.android.sdk.IALocationListener;
-
 public class Test {
 }
